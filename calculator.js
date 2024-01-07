cee5ea4de107ed199dfb4e4b5b649cba7fc960ca
marimuthu31 <154867366+marimuthu31@users.noreply.github.com>
2024-01-07 08:29:45 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,108 @@
+// clean=()=>{
+//     document.getElementById('displays').innerHTML='';
+// }
+// per=()=>{
+//     let per=document.getElementById('per').innerHTML;
+//     document.getElementById('displays').innerHTML+=per;
+// }
+// cancel=()=>{
+//    var a= document.getElementById('displays').innerHTML;
+//    b=a.slice(0,-1);
+//     document.getElementById('displays').innerHTML=b;
+// }
+// div=()=>{
+//     let div=document.getElementById('div').innerHTML;
+//     document.getElementById('displays').innerHTML+=div;
+// }
+// sev=()=>{
+//     let sev=document.getElementById('sev').innerHTML;
+
+//     document.getElementById('displays').innerHTML+=sev;
+// }
+// eig=()=>{
+//     let eig=document.getElementById('eig').innerHTML;
+//     document.getElementById('displays').innerHTML+=eig;
+// }
+// nine=()=>{
+//     let nine=document.getElementById('nine').innerHTML;
+//     document.getElementById('displays').innerHTML+=nine;
+// }
+// mul=()=>{
+//     let mul=document.getElementById('mul').innerHTML;
+//     document.getElementById('displays').innerHTML+=mul;
+// }
+
+// fou=()=>{
+//     let fou=document.getElementById('fou').innerHTML;
+//     document.getElementById('displays').innerHTML+=fou;
+// }
+
+// fiv=()=>{
+//     let fiv=document.getElementById('fiv').innerHTML;
+//     document.getElementById('displays').innerHTML+=fiv;
+// }
+
+// six=()=>{
+//     let six=document.getElementById('six').innerHTML;
+//     document.getElementById('displays').innerHTML+=six;
+// }
+
+// sub=()=>{
+//     let sub=document.getElementById('sub');
+//     document.getElementById('displays').innerHTML+=sub;
+// }
+// one=()=>{
+//     let one=document.getElementById('one').innerHTML;
+//     document.getElementById('displays').innerHTML+=one;
+// }
+// two=()=>{
+//     let two=document.getElementById('two').innerHTML;
+//     document.getElementById('displays').innerHTML+=two;
+// }
+// three=()=>{
+//     let three=document.getElementById('three').innerHTML;
+//     document.getElementById('displays').innerHTML+=three;
+// }
+// plus=()=>{
+//     let plus=document.getElementById('plus').innerHTML;
+//     document.getElementById('displays').innerHTML+=plus;
+// }
+// doublezero=()=>{
+//     let doublezero=document.getElementById('doublezero').innerHTML;
+//     document.getElementById('displays').innerHTML+=doublezero;
+// }
+// zero=()=>{
+//     let zero=document.getElementById('zero').innerHTML;
+//     document.getElementById('displays').innerHTML+=zero;
+// }
+// dot=()=>{
+//     let dot=document.getElementById('dot').innerHTML;
+//     document.getElementById('displays').innerHTML+=dot;
+// }
+// equal=()=>{
+//     document.getElementById('displays').innerHTML;
+///////////////////////////////////////////////////////////////////////////////////////////////// }
+//  var scr = document.getElementById("displays")
+// function display(num) {
+//     scr= num
+//     document.getElementById('displays').value+=scr
+// }
+// function calculate(){
+//     try{
+//         scr=eval(scr.value)
+//         document.getElementById('displays').value=scr
+//     }
+//     catch(err){
+//         alert('enter the valid input')
+//     }
+// }
+// function clear(){
+//     document.getElementById('displays').innerHTML=" "
+// }
+// function cle(){
+// aa=document.getElementById('displays').value;
+// bb=aa.slice(0,-1);
+// document.getElementById('displays').innerHTML=bb;
+// }/////////////////////////////////////////////////////////////////////////
+
+
